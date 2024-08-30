ce2342112ba07d2f899fea0620184c745252c32f
Thiago Magalhaes <thiago.magalhaes@gmail.com>
2024-08-30 20:40:47 -0300

    feat: Add IGW

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,6 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  
   cidr_block = "10.0.0.0/16"
 
   tags = {
@@ -15,6 +14,7 @@ resource "aws_subnet" "subnet1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "${var.region}a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "subnet-${var.region}a"
@@ -25,10 +25,42 @@ resource "aws_subnet" "subnet2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "${var.region}b"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "subnet-${var.region}b"
   }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "internet-gateway-${var.region}"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table-${var.region}"
+  }
+}
+
+resource "aws_route_table_association" "pub_subnet1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "pub_subnet2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_security_group" "sg" {
@@ -55,16 +87,37 @@ resource "aws_ecs_cluster" "main" {
   name = "${var.container_name}-cluster"
 }
 
-# Create the ECR repository
 resource "aws_ecr_repository" "repo" {
   name = "${var.container_name}"
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.container_name}-TaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_ecs_task_definition" "task" {
   family                   = "${var.container_name}-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   cpu                      = "256"
   memory                   = "512"
 
@@ -77,15 +130,6 @@ resource "aws_ecs_task_definition" "task" {
       hostPort      = 80
     }]
   }])
-}
-
-data "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
-  role       = data.aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_ecs_service" "service" {
