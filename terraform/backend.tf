378d6940e3430539f60b7dca95fb64d32b85d1ff
Thiago Magalhaes <thiago.magalhaes@gmail.com>
2024-09-05 16:56:12 -0300

    Make tf backend dynamic

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
     bucket         = "labsdevops-terraform-state-bucket"
-    key            = "live-demos/my-app/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-state-lock"
